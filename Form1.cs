bdf64f7f8e0c3a2d3f24c579de8c69e1fb9ee2ab
reality8080 <155755865+reality8080@users.noreply.github.com>
2025-03-18 11:50:32 +0700

    Add navbar effect

--- Form1.cs
+++ Form1.cs
@@ -10,18 +10,12 @@ namespace iBanking
         public static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
         [DllImport("user32.dll")]
         public static extern bool ReleaseCapture();
+
+        private List<Guna.UI2.WinForms.Guna2Button>? buttons;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void controlPageGunaPanel_MouseDown(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void mainDashGPanel_MouseDown(object sender, MouseEventArgs e)
-        {
 
         }
 
@@ -44,9 +38,81 @@ namespace iBanking
             }
         }
 
-        //private void homeGbtn_MouseClick(object sender, MouseEventArgs e)
-        //{
-        //    homeGbtn.BackColor = Color.FromArgb(14, 126, 254);
-        //}
+        private void homeGbtn_Click(object sender, EventArgs e)
+        {
+            setActiveBtns(homeGbtn,"D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\White\\homeWhite.png");
+            tabControlBank.SelectedIndex = 0;
+        }
+
+        private void myBanksGbtn_Click(object sender, EventArgs e)
+        {
+            setActiveBtns(myBanksGbtn, "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\White\\dollar512White.png");
+            tabControlBank.SelectedIndex = 1;
+        }
+
+        private void historyGbtn_Click(object sender, EventArgs e)
+        {
+            setActiveBtns(historyGbtn, "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\White\\historical512White.png");
+            tabControlBank.SelectedIndex = 2;
+
+        }
+
+        private void transferGBtn_Click(object sender, EventArgs e)
+        {
+            setActiveBtns(transferGBtn, "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\White\\prepaidCards512White.png");
+            tabControlBank.SelectedIndex = 3;
+        }
+
+        private void conncetBankGBtn_Click(object sender, EventArgs e)
+        {
+            setActiveBtns(conncetBankGBtn, "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\White\\cards-icon-size_512White.png");
+            tabControlBank.SelectedIndex = 4;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = new List<Guna.UI2.WinForms.Guna2Button>
+            {
+                homeGbtn,myBanksGbtn,historyGbtn,transferGBtn,conncetBankGBtn
+            };
+        }
+
+        private void ResestButtons()
+        {
+            buttons?.ForEach(button =>
+            {
+                foreach (var btn in buttons)
+                {
+                    btn.FillColor = Color.White;
+                    btn.BorderColor = Color.White;
+                    btn.ForeColor = Color.FromArgb(102, 112, 133);
+
+                    string iconPath = GetDefaultIconPath(btn);
+                    if (!string.IsNullOrEmpty(iconPath))
+                    {
+                        btn.Image = Image.FromFile(iconPath);
+                    }
+                }
+            });
+        }
+
+        private string GetDefaultIconPath(Guna.UI2.WinForms.Guna2Button btn)
+        {
+            if (btn == homeGbtn) return "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\Gray\\homeGray.png";
+            if (btn == myBanksGbtn) return "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\Gray\\dollar512Gray.png";
+            if (btn == historyGbtn) return "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\Gray\\historical512Gray.png";
+            if (btn == transferGBtn) return "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\Gray\\prepaidCards512Gray.png";
+            if (btn == conncetBankGBtn) return "D:\\2024-2025\\HK2\\WindowsForm\\Images\\Icon\\Gray\\cards-icon-size_512Gray.png";
+            return "";
+        }
+        private void setActiveBtns(Guna.UI2.WinForms.Guna2Button btn, string activeIconPath)
+        {
+            ResestButtons();
+            btn.FillColor = Color.FromArgb(14, 126, 254); 
+            btn.BorderColor = Color.FromArgb(14, 126, 254);
+            btn.ForeColor = Color.White;
+
+            btn.Image = Image.FromFile(activeIconPath);
+        }
     }
 }
